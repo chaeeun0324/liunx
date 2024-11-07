7e953dd5134b416c8316c1533503ca28ef647260
chaeeun0324 <codms9547@naver.com>
2024-11-07 10:54:33 +0900

    Add files via upload

---
+++ 241107/8-1/1/Shape1.h
@@ -0,0 +1,48 @@
+#ifndef SHAPE1_H
+#define SHAPE1_H
+
+#include <iostream>
+using namespace std;
+
+class Shape {
+private:
+    int x, y; // À§Ä¡ ÁÂÇ¥
+
+public:
+    Shape(int x = 0, int y = 0);
+    void setPosition(int x, int y);
+    void showPosition() const;
+};
+
+class Circle : public Shape {
+private:
+    int radius;
+
+public:
+    Circle(int x = 0, int y = 0, int radius = 1);
+    void setCircle(int x, int y, int radius);
+    void show() const;
+};
+
+class Rect : public Shape {
+private:
+    int width, height;
+
+public:
+    Rect(int x = 0, int y = 0, int width = 1, int height = 1);
+    void setRect(int x, int y, int width, int height);
+    void show() const;
+};
+
+class Triangle : public Shape {
+private:
+    int base, height;
+
+public:
+    Triangle(int x = 0, int y = 0, int base = 1, int height = 1);
+    void setTriangle(int x, int y, int base, int height);
+    void show() const;
+};
+
+#endif
+
